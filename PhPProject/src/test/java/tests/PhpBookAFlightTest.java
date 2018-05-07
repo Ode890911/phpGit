7312551ed87380e91ae2ddf76366f6644f9dbe50
mariode72 <you@example.com>
2018-05-07 00:06:16 -0400

    new

---
+++ PhPProject/src/test/java/tests/PhpBookAFlightTest.java
@@ -0,0 +1,22 @@
+package tests;
+
+import org.testng.annotations.Test;
+
+import com.custom.library.BasePage;
+
+import pages.HomePagePhP;
+
+public class PhpBookAFlightTest  extends BasePage{
+	
+	@Test
+	public void OneWayFlight() throws Exception{
+		
+		
+		//new HomePagePhP().goto_PhPeWebsite().ClickFlightLink().OneWayOption().DepartureCity().destCity().DepartDate().TravelerSelect().ClickSearchBtn();
+		new HomePagePhP().goto_PhPeWebsite().ClickFlightLink().OneWayOption().DepartureCity().destCity();
+	
+
+		
+		
+	}
+}
